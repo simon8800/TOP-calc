09b20905a3ebe61f650cbed8c9bd5a485e32a5e9
Simon Mei <smei8800@gmail.com>
2024-03-11 23:49:53 -0400

    Add logic for nums, signs, and operations. Need to add logic for equals

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ I NEED TO BUILD OUT THIS THING!
 */
 
 "use strict";
-
-// // function to change sign of number
-// function changeSign(num) {
-//     return num * -1;
-// }
-// // function to change to percentage
-// function percent(num) {
-//     return num / 100;
-// }
 // // function to divide numbers, be careful of dividing by 0
 // function divide(numOne, numTwo) {
 //     if (numTwo == 0) {
@@ -37,29 +28,123 @@ I NEED TO BUILD OUT THIS THING!
 // function add(numOne, numTwo) {
 //     return numOne + numTwo;
 // }
-// function to calculate results
 
-function evaluate() {}
-
-function allClear() {
-    return null;
+function changeDisplay(num) {
+    display.textContent = num;
 }
 
-function onClick(event) {
-    let value = event.target.dataset.val;
-    let lastIndex = "";
-    // if it's a number or decimal
-    if (numbers.includes(value) && value != ".") {
-        if (getNumTwo) {
-            operands["numTwo"] += value;
+function allClear() {
+    operands = [];
+    changeDisplay(0);
+}
+
+// changes sign or turns into a percentage
+function performOtherOp(op) {
+    if (operands.length < 3) {
+        if (op == "sign") {
+            operands[0] = changeSign(0);
         } else {
-            operands["numOne"] += value;
+            operands[0] = changePercent(0);
         }
     } else {
-        if (getNumTwo) {
+        if (op == "sign") {
+            operands[2] = changeSign(2);
+        } else {
+            operands[2] = changePercent(2);
         }
     }
 }
+
+function changeSign(index) {
+    if (operands[index].includes("-")) {
+        return operands[index].slice(1);
+    } else {
+        return "-" + operands[index];
+    }
+}
+
+function changePercent(index) {
+    let num = parseFloat(operands[index]);
+    let percentage = num / 100;
+    return String(percentage);
+}
+
+function modifyNum(num) {
+    let lastIndex = operands.length - 1;
+
+    if (!operands[lastIndex].includes(".") || numbers.includes(num)) {
+        operands[lastIndex] += num;
+    }
+
+    console.log(operands);
+    console.log(operands[lastIndex]);
+}
+
+function onClick(event) {
+    let val = event.target.dataset.val;
+    if (val == "AC") {
+        allClear();
+        return;
+    }
+
+    // if stack length is 0, either add a number or do nothing
+    if (operands.length == 0) {
+        if (numbers.includes(val) || val == ".") {
+            operands.push(val);
+        } else if (otherOps.includes(val)) {
+            operands.push("0");
+            performOtherOp(val);
+        } else if (operations.includes(val)) {
+        }
+        console.log(operands);
+        changeDisplay(operands[0]);
+        return;
+    }
+
+    // if the stack length is 1, then either add num or push operation or change sign or change percentage
+    if (operands.length == 1) {
+        if (numbers.includes(val) || val == ".") {
+            modifyNum(val);
+        } else if (operations.includes(val)) {
+            operands.push(val);
+        } else if (otherOps.includes(val)) {
+            performOtherOp(val);
+        }
+        changeDisplay(operands[0]);
+        return;
+    }
+
+    // if stack length is 2, then either add num or change operation or other op on first num
+    if (operands.length == 2) {
+        if (numbers.includes(val) || val == ".") {
+            operands.push(val);
+        } else if (operations.includes(val)) {
+            operands[1] = val;
+        } else if (otherOps.includes(val)) {
+            otherOps(val);
+        }
+        console.log(operands);
+        return;
+    }
+
+    // if array length is 3, then modify num, otherOp on second num, or
+    if (operands.length == 3) {
+        if (numbers.includes(val) || val == ".") {
+            modifyNum(val);
+        } else if (operations.includes(val)) {
+            // calculate and then replace operands[1]
+        } else if (otherOps.includes(val)) {
+            performOtherOp(val);
+        }
+        console.log(operands);
+        changeDisplay(operands[2]);
+        return;
+    }
+}
+
+
+
+
 
 let display = document.querySelector(".display");
 let buttons = document.querySelectorAll(".btn");
@@ -67,12 +152,9 @@ buttons.forEach((button) => {
     button.addEventListener("click", onClick);
 });
 
-let operands = {
-    numOne: "",
-    numTwo: "",
-    op: "",
-};
 let getNumTwo = false;
-let operations = "+-/*=";
-let nums = "01234567890.";
-let otherOperations = "sign%";
+let operations = "+-/*";
+let equals = "=";
+let numbers = "01234567890";
+let otherOps = "sign%";
+let operands = [];
